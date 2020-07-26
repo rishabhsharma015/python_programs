6e305a17efe49773fbf1ca3d3cef25df2d39b454
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2020-07-26 17:37:31 +0530

    Update Maximum_XOR[1].py

--- Maximum_XOR[1].py
+++ Maximum_XOR[1].py
@@ -1,8 +1,9 @@
+# maximum Xor.
 l1=[]
-l=list(map(int,input().split()))
+l=list(map(int,input().split()))   # list of integer
 for i in l:
 	for j in l:
-		k=i^j
+		k=i^j          # claculating the xor of elements
 		l1.append(k)
 l1.sort()
 t=l1.pop()
