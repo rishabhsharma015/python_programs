f954e18b75718dfb0288bac73610b965fe62d653
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2020-07-28 18:07:15 +0530

    done

---
+++ 3highestvaluesfromdictionary[1].py
@@ -0,0 +1,17 @@
+d={}
+l1=list(map(str,input().split()))
+for i in l1:
+	d[i]=int(input())
+	d.update({i:d[i]})
+print(d)
+l=list(d.values())
+l.sort()
+c=1
+print("The 3 highest values are-->")
+while c<4:
+	max=l.pop()
+	print(max)
+	c+=1
+
+
+	
